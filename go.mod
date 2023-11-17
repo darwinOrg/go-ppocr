a15974f4c9b02736cabd14bf0ee781d009682956
chenbiao <149027310@qq.com>
2023-11-17 14:26:48 +0800

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/darwinOrg/go-ppocr
 
 go 1.19
+
+require (
+	github.com/darwinOrg/go-common v0.0.25 // indirect
+	github.com/darwinOrg/go-logger v0.0.2 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
--- ocr.go
+++ ocr.go
@@ -1,18 +1,18 @@
 package dgocr
 
 import (
-	"fmt"
+	dgctx "github.com/darwinOrg/go-common/context"
+	dglogger "github.com/darwinOrg/go-logger"
 	"os/exec"
 )
 
-func OcrImageFile(sourceImageFile string, destJsonFile string) error {
+func OcrImageFile(ctx *dgctx.DgContext, sourceImageFile string, destJsonFile string) error {
 	cmd := exec.Command("python", "ocr.py", sourceImageFile, destJsonFile)
 	output, err := cmd.Output()
 	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Output:", string(output))
+		dglogger.Errorf(ctx, "exec python ocr.py error: %v", err)
+		return err
 	}
-
-	return err
+	dglogger.Debugf(ctx, "output: %s", string(output))
+	return nil
 }
